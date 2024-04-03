d3c9da8e93a5bb64f5f7d61cb20a3084e6855fc5
RoSantilli <rocioasantilli@gmail.com>
2024-04-03 19:22:22 -0300

    Añadiendo archivo

---
+++ Practica/src/practica/Practica.java
@@ -0,0 +1,18 @@
+
+package practica;
+
+
+public class Practica {
+
+    
+    public static void main(String[] args) {
+        
+        int numero = 5;
+        String nombre ="Molly";
+        char letra ='R';
+        double temperatura =34.45;
+        boolean SioNo = false;
+        long largo = 123456789;
+    }
+    
+}
